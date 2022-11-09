b7d08330febda90bde079744669e1ec38bd01644
Osaf <osaf96@gmail.com>
2022-11-09 19:10:34 +0500

    new commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 /*=============== SHOW SOCIAL NETWORKS ===============*/
+const showSocial = (toggle, specialCard) => {
+    const toggle = document.getElementById('toggleCard'),
+        social = document.getElementById('socialCard')
+
+    toggle.addEventListener('click', () => {
+        social.classList.toggle("animation")
+    })
+}
+showSocial("card-toggle", "card-social")
